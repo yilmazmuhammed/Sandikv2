3df87430c880854846fa3879bf35b2769d6368b3
Muhammed YILMAZ <yilmazmu15@itu.edu.tr>
2022-02-19 14:09:09 +0300

    Clock denemesi

--- sandik/utils/clock.py
+++ sandik/utils/clock.py
@@ -8,9 +8,19 @@ def timed_job():
     print('This job is run every 1 minutes.')
 
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=14, minute=10)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=14, minute=12)
 def scheduled_job():
-    print('This job is run every weekday at 5pm.')
+    print('This job is run every weekday at 5pm. 1')
+
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=11, minute=12)
+def scheduled_job2():
+    print('This job is run every weekday at 5pm. 2')
+
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=17, minute=12)
+def scheduled_job3():
+    print('This job is run every weekday at 5pm. 3')
 
 
 sched.start()
