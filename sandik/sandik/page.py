51d2eb196591265209d757697a354288407f75ad
Muhammed YILMAZ <yilmazmu15@itu.edu.tr>
2022-03-16 22:05:05 +0300

    Yetki düzenlemesi

--- sandik/sandik/page.py
+++ sandik/sandik/page.py
@@ -50,7 +50,7 @@ def sandik_detail_page(sandik_id):
     member = db.get_member(sandik_ref=g.sandik, web_user_ref=current_user)
     authority = current_user.get_sandik_authority(sandik=g.sandik)
     if not member and not authority and not current_user.is_admin():
-        abort(403)
+        abort(403, "Bu sayfayı görüntüleme yetkiniz bulunmamaktadır")
     return render_template("sandik/sandik_detail_page.html",
                            page_info=LayoutPI(title="Sandık detayı", active_dropdown="sandik"))
 
