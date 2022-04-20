6b2bba28cb9b9d3353aec6fa6858a3293b151dc8
Muhammed YILMAZ <yilmazmu15@itu.edu.tr>
2022-04-20 18:12:06 +0300

    NetGsmXmlApi'dan türkçe karakter desteği çıkartıldı, türkçe karakterler ingilizce karşılıklarına döüştürüldü

--- sandik/bot/netgsm/netgsm_xml.py
+++ sandik/bot/netgsm/netgsm_xml.py
@@ -4,6 +4,22 @@ import requests as requests
 
 from sandik.bot.netgsm.exceptions import InvalidLanguage
 
+not_latin_characters = {
+    ord('ı'): 'i',
+    ord('ş'): 's',
+    ord('ğ'): 'g',
+    ord('ü'): 'u',
+    ord('ç'): 'c',
+    ord('ö'): 'o',
+    ord('İ'): 'I',
+    ord('Ş'): 'S',
+    ord('Ğ'): 'G',
+    ord('Ü'): 'U',
+    ord('Ç'): 'C',
+    ord('Ö'): 'O',
+    ord('\n'): '\\n',
+}
+
 
 class NetGsmXmlApi:
     def __init__(self, usercode, password, appkey=None, default_message_header=None):
@@ -13,6 +29,7 @@ class NetGsmXmlApi:
         self.appkey = appkey
         self.default_message_header = default_message_header
         self.languages = ["TR"]
+        self.translate_dict = not_latin_characters
 
     def send_sms(self, message=None, phone_numbers=None, message_packages=None, message_header=None, language="TR"):
         if not message_packages and not message:
@@ -29,14 +46,14 @@ class NetGsmXmlApi:
         language_parameter = f'dil="{language}"' if language else ""
 
         if message:
-            message = message.replace("\n", "\\n")
+            message = message.translate(self.translate_dict)
             xml_body = f"""<msg><![CDATA[{message}]]></msg>"""
             for pn in phone_numbers:
                 xml_body += f"<no>{pn}</no>"
         else:
             xml_body = ""
             for mp in message_packages:
-                xml_body += f"""<mp><msg><![CDATA[{mp["message"]}]]></msg><no>{mp["phone_number"]}</no></mp>"""
+                xml_body += f"""<mp><msg><![CDATA[{mp["message"].translate(self.translate_dict)}]]></msg><no>{mp["phone_number"]}</no></mp>"""
 
         url = "https://api.netgsm.com.tr/sms/send/xml"
         payload = f"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n " \
@@ -65,10 +82,10 @@ if __name__ == '__main__':
     netgsm = NetGsmXmlApi(usercode=os.getenv("SMS_BOT_USERCODE"),
                           password=os.getenv("SMS_BOT_PASSWORD"),
                           default_message_header=os.getenv("SMS_BOT_DEFAULT_MESSAGE_HEADER"))
-    # resp = netgsm.send_sms("mesaj\n1", phone_numbers=["905392024175"])
-    # print("response:", resp)
-    # print("response.text:", resp.text)
-    resp = netgsm.send_sms(message_packages=[{"message": str(datetime.now()) + "\\n" + str(datetime.now()),
-                                              "phone_number": "905392024175"}])
+    resp = netgsm.send_sms("qwertyuıopğüasdfghjklşizxcvbnmöç\nQWERTYUIOPĞÜİŞLKJHGFDSAZXCVBNMÖÇ", phone_numbers=["905392024175"])
     print("response:", resp)
     print("response.text:", resp.text)
+    # resp = netgsm.send_sms(message_packages=[{"message": "Güven bağı " + str(datetime.now()) + "\\n" + str(datetime.now()),
+    #                                           "phone_number": "905392024175"}])
+    # print("response:", resp)
+    # print("response.text:", resp.text)
