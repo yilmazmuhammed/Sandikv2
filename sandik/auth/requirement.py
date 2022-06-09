619eb5f17a192ae8dd7ba94d30ec9daec59245af
Muhammed YILMAZ <mhyilmaz97@gmail.com>
2022-06-09 09:12:01 +0300

    Bazı python modüllerinin versiyonları güncellendi. (requirements.txt) Sanal ortamda kurulu olan bütün python modüllerin ve versiyonlarının bulunduğu requirements_full.txt dosyası eklendi Flask-Login modülündeki hata sebebiyle tekrar yazılan @login_required modülde düzeltme yapıldığından ötürü kaldırıldı.

--- sandik/auth/requirement.py
+++ sandik/auth/requirement.py
@@ -1,27 +1,9 @@
 from functools import wraps
 
-from flask import abort, current_app, request, g
-from flask_login import current_user, logout_user
-from flask_login.config import EXEMPT_METHODS
+from flask import abort, g
+from flask_login import current_user, login_required
 
 from sandik.auth import db
-
-
-def login_required(func):
-    @wraps(func)
-    def decorated_view(*args, **kwargs):
-        if request.method in EXEMPT_METHODS:
-            return func(*args, **kwargs)
-        elif current_app.config.get('LOGIN_DISABLED'):
-            return func(*args, **kwargs)
-        elif not current_user.is_authenticated:
-            return current_app.login_manager.unauthorized()
-        elif not current_user.is_active:
-            logout_user()
-            return current_app.login_manager.unauthorized()
-        return func(*args, **kwargs)
-
-    return decorated_view
 
 
 def admin_required(func):
