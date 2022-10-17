c71b43b662155e694d275312aa59c0706238ef5e
Muhammed YILMAZ <yilmazmu15@itu.edu.tr>
2022-10-17 15:37:18 +0300

    İşlem ekleme sayfasındaki tarih kısmının eski tarihi gösterme hatası düzeltildi

--- sandik/transaction/page.py
+++ sandik/transaction/page.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Blueprint, request, render_template, g, flash, url_for, abort
 from flask_login import current_user
 from pony.orm import desc, rollback
@@ -51,6 +53,8 @@ def add_money_transaction_by_manager_page(sandik_id):
             raise e
             rollback()
             flash(str(e), "danger")
+    elif not form.is_submitted():
+        form.date.data = datetime.today()
 
     return render_template("transaction/add_money_transaction_by_manager_page.html",
                            page_info=FormPI(title="Para giriş/çıkışı ekle", form=form,
